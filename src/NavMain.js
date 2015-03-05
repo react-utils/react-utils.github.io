a54dca68e27abfefc62e02a56d25b5d521acfe30
Sahusoft <suraj@sahusoft.com>
2015-03-05 21:00:24 +0530

    Minor Fixes

--- src/NavMain.js
+++ src/NavMain.js
@@ -26,7 +26,7 @@ var NavMain = React.createClass({
     return (
       <Navbar componentClass='header' brand={brand} staticTop className="bs-docs-nav" role="banner" toggleNavKey={0}>
         <Nav className="bs-navbar-collapse" role="navigation" eventKey={0} id="top">
-          {Object.keys(NAV_LINKS).map(this.renderNavItem)}
+          <li><a href="/react-utils-helper">Component Helper</a></li>
         </Nav>
       </Navbar>
     );
